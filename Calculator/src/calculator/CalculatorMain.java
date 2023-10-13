b1c9659295037b116b13830a27711521a6cfc826
paulinerougeot <paulinerougeot@localhost>
2023-10-13 15:37:52 +0200

    Main modifié

--- Calculator/src/calculator/CalculatorMain.java
+++ Calculator/src/calculator/CalculatorMain.java
@@ -1,10 +1,15 @@
 package calculator;
 
+import java.util.Stack;
+
 public class CalculatorMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Stack acc= new Stack<Integer>();
+		acc.push(1);
+		acc.push(2);
+		System.out.println(acc);
 	}
 
 }
