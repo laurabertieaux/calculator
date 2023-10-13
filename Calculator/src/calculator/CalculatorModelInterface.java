6c3a56749e17c76ecadedd51070c790da31d38f9
paulinerougeot <paulinerougeot@localhost>
2023-10-13 14:19:37 +0200

    Nouvelles classes

---
+++ Calculator/src/calculator/CalculatorModelInterface.java
@@ -0,0 +1,45 @@
+package calculator;
+
+public class CalculatorModelInterface {
+
+	public void add() {
+		
+	}
+	
+	public void substract() {
+		
+	}
+	
+	public void multiply() {
+		
+	}
+	
+	public void divide() {
+		
+	}
+	
+	public void opposite() {
+		
+	}
+	
+	public void push() {
+		
+	}
+	
+	public void pop() {
+		
+	}
+	
+	public void drop() {
+		
+	}
+	
+	public void swap() {
+		
+	}
+	
+	public void clear() {
+		
+	}
+
+}
