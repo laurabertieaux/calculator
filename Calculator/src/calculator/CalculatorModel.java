9842f83fb1dbe36f98a03a6aaa2f1aef3ea254aa
paulinerougeot <paulinerougeot@localhost>
2023-10-18 14:24:19 +0200

    pile en accu

--- Calculator/src/calculator/CalculatorModel.java
+++ Calculator/src/calculator/CalculatorModel.java
@@ -19,9 +19,9 @@ public class CalculatorModel implements CalculatorModelInterface {
 		// TODO Auto-generated method stub
 		Double a;
 		Double b;
-		a = accu.pop();
-		b = accu.pop();
-		accu.push(a+b);
+		a = pile.pop();
+		b = pile.pop();
+		pile.push(a+b);
 		
 	}
 
@@ -30,9 +30,9 @@ public class CalculatorModel implements CalculatorModelInterface {
 		// TODO Auto-generated method stub
 		double a;
 		double b;
-		a = accu.pop();
-		b = accu.pop();
-		accu.push(b-a);
+		a = pile.pop();
+		b = pile.pop();
+		pile.push(b-a);
 	}
 
 	@Override
