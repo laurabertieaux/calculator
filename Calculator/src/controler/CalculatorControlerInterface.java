37da344b115e960a83dc8351ca07192c0d9dffa5
paulinerougeot <paulinerougeot@localhost>
2023-10-20 15:12:02 +0200

    création controleur

---
+++ Calculator/src/controler/CalculatorControlerInterface.java
@@ -0,0 +1,10 @@
+package controler;
+
+import java.util.List;
+
+public interface CalculatorControlerInterface {
+	
+	public void change(String accu);
+	public void change(List<Double> stackData);
+
+}
