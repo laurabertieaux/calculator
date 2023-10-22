cd97d887dcfbf2280c5cbd492d1c1d450444d587
NapsyGit <alban.bertieaux@gmail.com>
2023-10-22 19:59:20 +0200

    on action

--- Calculator/src/controler/CalculatorControler.java
+++ Calculator/src/controler/CalculatorControler.java
@@ -2,6 +2,8 @@ package controler;
 
 import java.util.List;
 
+import javafx.event.ActionEvent;
+
 public class CalculatorControler implements CalculatorControlerInterface {
 
 	@Override
@@ -15,6 +17,44 @@ public class CalculatorControler implements CalculatorControlerInterface {
 		// TODO Auto-generated method stub
 		
 	}
+	public void btn1 (ActionEvent event) {
+		
+	}
 	
+	public void btn2 (ActionEvent event) {
+		
+	}
+	
+	public void btn3 (ActionEvent event) {
+		
+	}
+	
+	public void btn4 (ActionEvent event) {
+		
+	}
+	
+	public void btn5 (ActionEvent event) {
+		
+	}
+	
+	public void btn6 (ActionEvent event) {
+		
+	}
+
+	public void btn7 (ActionEvent event) {
+	
+	}
+
+	public void btn8 (ActionEvent event) {
+	
+	}
+	
+	public void btn9 (ActionEvent event) {
+		
+	}
+	
+	public void btn0 (ActionEvent event) {
+		
+	}
 
 }
