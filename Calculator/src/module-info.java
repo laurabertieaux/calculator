1e93b83b55cbe52ce817c0cefbb51992c9e9b5f1
paulinerougeot <paulinerougeot@localhost>
2023-10-22 19:17:26 +0200

    Merge branch 'main' of https://github.com/laurabertieaux/calculator.git into main

--- Calculator/src/module-info.java
+++ Calculator/src/module-info.java
@@ -8,6 +8,7 @@ module Calculator {
 	requires javafx.graphics;
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires java.desktop;
 	opens view to javafx.graphics, javafx.fxml ;
 	//opens controler to javafx.fxml;
 }
