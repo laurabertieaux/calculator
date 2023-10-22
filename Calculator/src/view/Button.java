01ebdd9bf5236a0cac5ac67ddbd67ad3321305d3
paulinerougeot <paulinerougeot@localhost>
2023-10-22 19:21:08 +0200

    import classe button

---
+++ Calculator/src/view/Button.java
@@ -0,0 +1,33 @@
+package view;
+
+import java.awt.event.ActionListener;
+
+public class Button {
+	
+	public void ajout_accumulateur() {
+		
+	}
+	
+	public void somme() {
+		
+	}
+	
+	public void difference() {
+		
+	}
+	
+	public void quotient() {
+		
+	}
+	
+	public void signe() {
+		
+	}
+	
+	public void produit() {
+		
+		
+	}
+	
+
+}
