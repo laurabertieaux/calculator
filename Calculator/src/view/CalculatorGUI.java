bbbd8b153cd0e10a9a9a2ceef3920189e892506e
NapsyGit <alban.bertieaux@gmail.com>
2023-10-22 18:40:35 +0200

    ca remarche youpiiiii

--- Calculator/src/view/CalculatorGUI.java
+++ Calculator/src/view/CalculatorGUI.java
@@ -25,9 +25,9 @@ public class CalculatorGUI extends Application {
         primaryStage.show();
     }
 
-    public static void main(String[] args)      {
+    public static void main(String[] args) {
         launch(args);
     }
- 
+
 
 }
