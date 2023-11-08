d1d08c03d18db2a1244ac493aac4baa79659d3eb
paulinerougeot <paulinerougeot@mbp-de-pauline>
2023-11-08 16:18:44 +0100

    comm module-info

--- Calculator/src/module-info.java
+++ Calculator/src/module-info.java
@@ -1,15 +1,12 @@
 /**
  * 
  */
-/**
- * 
- */
 module Calculator { 
-	exports calculator;
+	exports calculator; // 
 	requires javafx.graphics;
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires java.desktop;
-	opens view to javafx.graphics, javafx.fxml ;
-	opens controler to javafx.fxml; 
+	opens view to javafx.graphics, javafx.fxml ; // fait le lien entre le package view, javafx.fxml et javafx.graphics (qui gère les événements graphiques comme les interactions entre l'utilisateur et l'interface graphique 
+	opens controler to javafx.fxml; // fait le lien entre le package controler et javafx.fxml (langage pour décrire l'interface graphique)
 }
