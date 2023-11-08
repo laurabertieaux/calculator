56d1e83005e2cc85813f6c3ce8693552d93eaea0
paulinerougeot <paulinerougeot@mbp-de-pauline>
2023-11-08 16:02:10 +0100

    Comm CalculatorControlerInterface

--- Calculator/src/controler/CalculatorControlerInterface.java
+++ Calculator/src/controler/CalculatorControlerInterface.java
@@ -4,7 +4,18 @@ import java.util.List;
 
 public interface CalculatorControlerInterface {
 	
+	/**
+	 * Méthode que nous avons choisi de ne pas utiliser dans notre projet
+	 * Nous pensons quelle permet de mettre à jour la valeur de l'accumulateur
+	 * @param accu nouvelle valeur de l'accumulateur
+	 */
 	public void change(String accu);
+	
+	/**
+	 * Méthode que nous avons choisi de ne pas utiliser dans notre projet
+	 * Nous pensons quelle permet de mettre à jour la valeur de la pile
+	 * @param stackData nouvelle valeur de la pile
+	 */
 	public void change(List<Double> stackData);
  
 }
